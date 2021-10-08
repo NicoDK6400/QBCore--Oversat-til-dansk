35e370b2ee593956fc16d7d07677cd534e78ffa8
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-10-08 21:59:37 +0200

    Added cars
    I've added some police cars (danish)
    - Only for ELS test (you can keep them if you want, from my old server)

---
+++ resources/[biler]/Vehicles-Emergency/__resource.lua
@@ -0,0 +1,133 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+
+-- EMS/POLITI Helikopter | Spawn name: POLMAV
+files {
+    'data/POLMAV/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/POLMAV/handling.meta'
+
+
+
+-- Yamaha FJR1300 (2009) Politi Motorcykel | Spawn name: YF2009PB
+files {
+    'data/YF2009PB/vehicles.meta',
+    'data/YF2009PB/carvariations.meta',
+    'data/YF2009PB/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/YF2009PB/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/YF2009PB/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/YF2009PB/carvariations.meta'
+
+
+-- Volvo XC90 (2017) Politi | Spawn name: VXC902017P
+files {
+    'data/VXC902017P/vehicles.meta',
+    'data/VXC902017P/carvariations.meta',
+    'data/VXC902017P/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/VXC902017P/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/VXC902017P/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/VXC902017P/carvariations.meta'
+
+
+-- Karin Sultan RS (2018) Civilpoliti | Spawn name: KSRS2018U
+files {
+    'data/KSRS2018U/vehicles.meta',
+    'data/KSRS2018U/carvariations.meta',
+    'data/KSRS2018U/carcols.meta',
+    'data/KSRS2018U/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/KSRS2018U/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/KSRS2018U/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/KSRS2018U/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/KSRS2018U/carvariations.meta'
+
+
+-- Lenco Bearcat (2018) AKS | Spawn name: LB2018AKS
+files {
+    'data/LB2018AKS/vehicles.meta',
+    'data/LB2018AKS/carvariations.meta',
+    'data/LB2018AKS/carcols.meta',
+    'data/LB2018AKS/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/LB2018AKS/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/LB2018AKS/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/LB2018AKS/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/LB2018AKS/carvariations.meta'
+
+
+-- Bravado Buffalo (2018) Civilpoliti | Spawn Name: BB2018U
+files {
+    'data/BB2018U/vehicles.meta',
+    'data/BB2018U/carvariations.meta',
+    'data/BB2018U/carcols.meta',
+    'data/BB2018U/handling.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/BB2018U/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/BB2018U/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/BB2018U/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/BB2018U/carvariations.meta'
+
+
+-- Ubermacht Oracle (2018) Civilpoliti | Spawn Name: UO2018U
+files {
+    'data/UO2018U/vehicles.meta',
+    'data/UO2018U/carvariations.meta',
+    'data/UO2018U/carcols.meta',
+    'data/UO2018U/handling.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/UO2018U/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/UO2018U/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/UO2018U/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/UO2018U/carvariations.meta'
+
+
+-- Ubermacht Oracle XS (2018) Civilpoliti | Spawn Name: UOXS2018U
+files {
+    'data/UOXS2018U/vehicles.meta',
+    'data/UOXS2018U/carvariations.meta',
+    'data/UOXS2018U/carcols.meta',
+    'data/UOXS2018U/handling.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/UOXS2018U/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/UOXS2018U/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/UOXS2018U/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/UOXS2018U/carvariations.meta'
+
+
+-- Nissan GTR (2017) Civilpoliti Fartjagt | Spawn name: NGTR2017U
+
+files {
+    'data/NGTR2017U/vehicles.meta',
+    'data/NGTR2017U/carvariations.meta',
+    'data/NGTR2017U/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/NGTR2017U/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/NGTR2017U/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/NGTR2017U/carvariations.meta'
+
+
+data_file 'HANDLING_FILE' 'data/FIOR2019P/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/FIOR2019P/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/FIOR2019P/carvariations.meta'
+
+
+-- Passat | Spawn name: police3
+
+files {
+  'data/POLICE3/handling.meta',
+  --'data/sheriff/vehicles.meta',
+
+}
+data_file 'HANDLING_FILE' 'data/POLICE3/handling.meta'
+--data_file 'VEHICLE_METADATA_FILE' 'data/sheriff/vehicles.meta'
