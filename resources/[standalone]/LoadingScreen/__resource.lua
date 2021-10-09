b03427c331d4dee0471596580a0e7a3b014b3264
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-10-09 17:54:41 +0200

    LoadingScreen added
    A small minigame as an Loadingscreen
    (OPTIONAL)

---
+++ resources/[standalone]/LoadingScreen/__resource.lua
@@ -0,0 +1,11 @@
+loadscreen 'LoadingScreen/index.html'
+
+files {
+  'LoadingScreen/index.html',
+  'LoadingScreen/index.css',
+  'LoadingScreen/index.js',
+  'LoadingScreen/seta.png',
+  'LoadingScreen/dwuseta.png',
+  'LoadingScreen/logo.png',
+  'LoadingScreen/fonts/nfs.ttf'
+}
