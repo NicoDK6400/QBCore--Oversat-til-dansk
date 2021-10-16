41fb8bf8e62851d0ef30d566bbdb2d6dd3cbd18a
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-10-16 13:58:31 +0200

    Added Tattoo shop
    I don't know why this wasn't within the files, but now it's here!
    Tattoo shop is added and translated!

---
+++ resources/[qb]/qb-tattooshop_1/server/main.lua
@@ -0,0 +1,55 @@
+QBCore = nil
+TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+
+-- Code
+
+QBCore.Functions.CreateCallback('qb-tattooshop:GetPlayerTattoos', function(source, cb)
+    local Player = QBCore.Functions.GetPlayer(source)
+
+	if Player then
+		 exports['ghmattimysql']:execute('SELECT tattoos FROM playerskins WHERE citizenid = @citizenid', {
+			['@citizenid'] = Player.PlayerData.citizenid
+		}, function(result)
+			if result[1].tattoos then
+				cb(json.decode(result[1].tattoos))
+			else
+				cb()
+			end
+		end)
+	else
+		cb()
+	end
+end)
+
+QBCore.Functions.CreateCallback('qb-tattooshop:PurchaseTattoo', function(source, cb, tattooList, price, tattoo, tattooName)
+	local Player = QBCore.Functions.GetPlayer(source)
+
+
+	if Player.PlayerData.money.cash >= price then
+        Player.Functions.RemoveMoney('cash', price)
+
+		table.insert(tattooList, tattoo)
+
+		 exports['ghmattimysql']:execute('UPDATE playerskins SET tattoos = @tattoos WHERE citizenid = @citizenid', {
+			['@tattoos'] = json.encode(tattooList),
+			['@citizenid'] = Player.PlayerData.citizenid
+		})
+
+		TriggerClientEvent('QBCore:Notify', source, "Du købte: " .. tattooName .. " for DKK" .. price, 'success')
+		cb(true)
+	else
+		TriggerClientEvent('QBCore:Notify', source, "Du har ikke penge nok til denne tatovering", 'success')
+
+		cb(false)
+	end
+end)
+
+RegisterServerEvent('qb-tattooshop:server:RemoveTattoo')
+AddEventHandler('qb-tattooshop:server:RemoveTattoo', function (tattooList)
+	local Player = QBCore.Functions.GetPlayer(source)
+
+	exports['ghmattimysql']:execute('UPDATE playerskins SET tattoos = @tattoos WHERE citizenid = @citizenid', {
+		['@tattoos'] = json.encode(tattooList),
+		['@citizenid'] = Player.PlayerData.citizenid
+	})
+end)
