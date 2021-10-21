7aaa9b760f7d3dca82bc1d3eb4faf840053016a9
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-10-21 21:51:28 +0200

    Updated http
    some links where in http. changed it to https

--- resources/[cfx-default]/[gameplay]/chat/html/js/App.js
+++ resources/[cfx-default]/[gameplay]/chat/html/js/App.js
@@ -23,7 +23,7 @@ window.APP = {
     window.removeEventListener('message', this.listener);
   },
   mounted() {
-    post('http://chat/loaded', JSON.stringify({}));
+    post('https://chat/loaded', JSON.stringify({}));
     this.listener = window.addEventListener('message', (event) => {
       const item = event.data || event.detail; //'detail' is for debuging via browsers
       if (this[item.type]) {
@@ -244,7 +244,7 @@ window.APP = {
     },
     send(e) {
       if(this.message !== '') {
-        post('http://chat/chatResult', JSON.stringify({
+        post('https://chat/chatResult', JSON.stringify({
           message: this.message,
         }));
         this.oldMessages.unshift(this.message);
@@ -256,7 +256,7 @@ window.APP = {
     },
     hideInput(canceled = false) {
       if (canceled) {
-        post('http://chat/chatResult', JSON.stringify({ canceled }));
+        post('https://chat/chatResult', JSON.stringify({ canceled }));
       }
       this.message = '';
       this.showInput = false;
