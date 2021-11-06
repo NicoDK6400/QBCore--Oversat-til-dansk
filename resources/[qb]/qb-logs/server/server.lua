fb2b7bc0b51d1000cc62d648af9a3903287d62c9
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-06 18:59:20 +0100

    Updated qb-logs
    Minor adjustments

--- resources/[qb]/qb-logs/server/server.lua
+++ resources/[qb]/qb-logs/server/server.lua
@@ -16,29 +16,29 @@ Vores sider:
 
 RegisterServerEvent('qb-log:server:CreateLog')
 AddEventHandler('qb-log:server:CreateLog', function(name, title, color, message, tagEveryone)
-    local tag = tagEveryone ~= nil and tagEveryone or false
-    local webHook = Config.Webhooks[name] ~= nil and Config.Webhooks[name] or Config.Webhooks["default"]
+    local tag = tagEveryone or false
+    local webHook = Config.Webhooks[name] or Config.Webhooks['default']
     local embedData = {
         {
-            ["title"] = title,
-            ["color"] = Config.Colors[color] ~= nil and Config.Colors[color] or Config.Colors["default"],
-            ["footer"] = {
-                ["text"] = os.date("%c"),
+            ['title'] = title,
+            ['color'] = Config.Colors[color] or Config.Colors['default'],
+            ['footer'] = {
+                ['text'] = os.date('%c'),
             },
-            ["description"] = message,
-            ["author"] = {
-            ["name"] = 'Server Logs',
-            ["icon_url"] = "https://cdn.discordapp.com/attachments/870094209783308299/870104723338973224/Logotype_-_Display_Picture_-_Stylized_-_Red.png",
-                },
+            ['description'] = message,
+            ['author'] = {
+                ['name'] = 'QBCore Logs',
+                ['icon_url'] = 'https://media.discordapp.net/attachments/870094209783308299/870104331142189126/Logo_-_Display_Picture_-_Stylized_-_Red.png?width=670&height=670',
+            },
         }
     }
-    PerformHttpRequest(webHook, function(err, text, headers) end, 'POST', json.encode({ username = "Server Logs",embeds = embedData}), { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest(webHook, function(err, text, headers) end, 'POST', json.encode({ username = 'QB Logs', embeds = embedData}), { ['Content-Type'] = 'application/json' })
     Citizen.Wait(100)
     if tag then
-        PerformHttpRequest(webHook, function(err, text, headers) end, 'POST', json.encode({ username = "Server Logs", content = "@everyone"}), { ['Content-Type'] = 'application/json' })
+        PerformHttpRequest(webHook, function(err, text, headers) end, 'POST', json.encode({ username = 'QB Logs', content = '@everyone'}), { ['Content-Type'] = 'application/json' })
     end
 end)
 
-QBCore.Commands.Add("testwebhook", "Test dine Discord webhook for logs (Kun God)", {}, false, function(source, args)
-    TriggerEvent("qb-log:server:CreateLog", "default", "TestWebhook", "default", "Triggered **a** test webhook :)")
-end, "god")
+QBCore.Commands.Add('testwebhook', 'Test Your Discord Webhook For Logs (God Only)', {}, false, function(source, args)
+    TriggerEvent('qb-log:server:CreateLog', 'testwebhook', 'Test Webhook', 'default', 'Webhook setup successfully')
+end, 'god')
