20f8f7d18b8f2ee6a32d65cfda6f5266ce826930
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-11 21:06:03 +0100

    Added some Maps/Props
    Added maps/props
    - Center
    - PD (upgrade)
    - Helipad
    - Bahama Mamma
    It's called Random maps, as the name describes, it's just a lot of maps/props...

---
+++ resources/[maps]/random_maps/fxmanifest.lua
@@ -0,0 +1,19 @@
+--[[
+ ______ _           __  __      _ _    
+|  ____(_)         |  \/  |    | | |   
+| |__   ___   _____| \  / |  __| | | __
+|  __| | \ \ / / _ \ |\/| | / _` | |/ /
+| |    | |\ V /  __/ |  | || (_| |   < 
+|_|    |_| \_/ \___|_|  |_(_)__,_|_|\_\
+
+Vores sider:
+  • Hjemmesiden: https://fivem.dk
+  • Patreon: https://patreon.com/dkfivem
+  • Facebook: https://facebook.com/dkfivem
+  • Discord: https://discord.gg/dkfivem
+  • DybHosting: https://dybhosting.eu/ - Rabatkode: dkfivem10
+]]
+
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
