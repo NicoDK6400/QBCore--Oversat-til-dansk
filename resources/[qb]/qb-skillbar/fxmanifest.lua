102a83c001a9da08bf045f3b96e7a3ac6e3a0fb3
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-15 19:47:32 +0100

    Added Export
    local QBCore = exports['qb-core']:GetCoreObject()

--- resources/[qb]/qb-skillbar/fxmanifest.lua
+++ resources/[qb]/qb-skillbar/fxmanifest.lua
@@ -21,7 +21,6 @@ description 'QB-Skillbar'
 version '1.0.0'
 
 ui_page "html/index.html"
-shared_script '@qb-core/import.lua'
 client_script 'client/main.lua'
 
 files {
@@ -38,3 +37,5 @@ exports {
 dependencies {
     'qb-core'
 }
+
+lua54 'yes'
