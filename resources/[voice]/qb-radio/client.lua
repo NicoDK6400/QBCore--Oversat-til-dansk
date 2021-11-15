238166127dccbb4502c1ef213779d0330daeef5b
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-15 19:51:42 +0100

    Added Export
    local QBCore = exports['qb-core']:GetCoreObject()

--- resources/[voice]/qb-radio/client.lua
+++ resources/[voice]/qb-radio/client.lua
@@ -14,6 +14,8 @@ Vores sider:
   • DybHosting: https://dybhosting.eu/ - Rabatkode: dkfivem10
 ]]
 
+local QBCore = exports['qb-core']:GetCoreObject()
+
 local radioMenu = false
 local isLoggedIn = false
 local onRadio = false
