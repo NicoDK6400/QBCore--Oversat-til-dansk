19d5008c77d38b2d8a28cc77e44969376eb2cd7e
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-15 19:40:55 +0100

    Added Export
    local QBCore = exports['qb-core']:GetCoreObject()

--- resources/[qb]/qb-garages/client/main.lua
+++ resources/[qb]/qb-garages/client/main.lua
@@ -14,6 +14,8 @@ Vores sider:
   • DybHosting: https://dybhosting.eu/ - Rabatkode: dkfivem10
 ]]
 
+local QBCore = exports['qb-core']:GetCoreObject()
+
 local currentHouseGarage = nil
 local hasGarageKey = nil
 local currentGarage = nil
