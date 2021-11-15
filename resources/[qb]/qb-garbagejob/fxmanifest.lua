497f8caa39cf6c4b18f8f5b63f1f8e1c81e07c32
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-15 19:41:07 +0100

    Added Export
    local QBCore = exports['qb-core']:GetCoreObject()

--- resources/[qb]/qb-garbagejob/fxmanifest.lua
+++ resources/[qb]/qb-garbagejob/fxmanifest.lua
@@ -20,10 +20,9 @@ game 'gta5'
 description 'QB-GarbageJob'
 version '1.0.0'
 
-shared_scripts { 
-	'@qb-core/import.lua',
-	'config.lua'
-}
+shared_script 'config.lua'
 
 client_script 'client/main.lua'
 server_script 'server/main.lua'
+
+lua54 'yes'
--- resources/[qb]/qb-garbagejob/server/main.lua
+++ resources/[qb]/qb-garbagejob/server/main.lua
@@ -14,6 +14,8 @@ Vores sider:
   • DybHosting: https://dybhosting.eu/ - Rabatkode: dkfivem10
 ]]
 
+local QBCore = exports['qb-core']:GetCoreObject()
+
 local Bail = {}
 
 QBCore.Functions.CreateCallback('qb-garbagejob:server:HasMoney', function(source, cb)
