742c402e912f4977c2c9ed635cdbd1797a522611
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-15 19:32:47 +0100

    Added Export
    local QBCore = exports['qb-core']:GetCoreObject()

--- resources/[qb]/qb-anticheat/server/main.lua
+++ resources/[qb]/qb-anticheat/server/main.lua
@@ -14,6 +14,8 @@ Vores sider:
   • DybHosting: https://dybhosting.eu/ - Rabatkode: dkfivem10
 ]]
 
+local QBCore = exports['qb-core']:GetCoreObject()
+
 -- Get permissions --
 
 QBCore.Functions.CreateCallback('qb-anticheat:server:GetPermissions', function(source, cb)
