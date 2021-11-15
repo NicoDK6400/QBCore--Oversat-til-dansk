f527d5abf5cc0596dfc915cbddf76c9aab171a39
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-15 19:34:04 +0100

    Added Export
    local QBCore = exports['qb-core']:GetCoreObject()

--- resources/[qb]/qb-bankrobbery/server/main.lua
+++ resources/[qb]/qb-bankrobbery/server/main.lua
@@ -14,6 +14,8 @@ Vores sider:
   • DybHosting: https://dybhosting.eu/ - Rabatkode: dkfivem10
 ]]
 
+local QBCore = exports['qb-core']:GetCoreObject()
+
 local robberyBusy = false
 local timeOut = false
 local blackoutActive = false
