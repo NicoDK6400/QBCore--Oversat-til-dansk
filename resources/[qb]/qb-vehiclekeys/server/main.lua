3d1dd22d6c33fef710ee4e9d6042817dd4ab843a
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-15 19:49:43 +0100

    Added Export
    local QBCore = exports['qb-core']:GetCoreObject()

--- resources/[qb]/qb-vehiclekeys/server/main.lua
+++ resources/[qb]/qb-vehiclekeys/server/main.lua
@@ -14,6 +14,7 @@ Vores sider:
   • DybHosting: https://dybhosting.eu/ - Rabatkode: dkfivem10
 ]]
 
+local QBCore = exports['qb-core']:GetCoreObject()
 local VehicleList = {}
 
 QBCore.Functions.CreateCallback('vehiclekeys:CheckOwnership', function(source, cb, plate)
