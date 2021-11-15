7427d6f50f1f50da504007c1c36a37df173d6cd6
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-15 19:35:19 +0100

    Added Export
    local QBCore = exports['qb-core']:GetCoreObject()

--- resources/[qb]/qb-builderjob/server/main.lua
+++ resources/[qb]/qb-builderjob/server/main.lua
@@ -14,6 +14,8 @@ Vores sider:
   • DybHosting: https://dybhosting.eu/ - Rabatkode: dkfivem10
 ]]
 
+local QBCore = exports['qb-core']:GetCoreObject()
+
 QBCore.Functions.CreateCallback('qb-builderjob:server:GetCurrentProject', function(source, cb)
     local CurProject = nil
     for k, v in pairs(Config.Projects) do
