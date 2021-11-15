567800391366d12207bbd3b227a22e249d42f0d5
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-15 19:49:03 +0100

    Added Export
    local QBCore = exports['qb-core']:GetCoreObject()

--- resources/[qb]/qb-traphouse/server/main.lua
+++ resources/[qb]/qb-traphouse/server/main.lua
@@ -14,6 +14,8 @@ Vores sider:
   • DybHosting: https://dybhosting.eu/ - Rabatkode: dkfivem10
 ]]
 
+local QBCore = exports['qb-core']:GetCoreObject()
+
 RegisterServerEvent('qb-traphouse:server:TakeoverHouse')
 AddEventHandler('qb-traphouse:server:TakeoverHouse', function(Traphouse)
     local src = source
