4a0e28c65de2771193342537e2218091d43bfa75
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-15 19:46:39 +0100

    Added Export
    local QBCore = exports['qb-core']:GetCoreObject()

--- resources/[qb]/qb-radialmenu/client/main.lua
+++ resources/[qb]/qb-radialmenu/client/main.lua
@@ -14,6 +14,7 @@ Vores sider:
   • DybHosting: https://dybhosting.eu/ - Rabatkode: dkfivem10
 ]]
 
+QBCore = exports['qb-core']:GetCoreObject()
 local inRadialMenu = false
 
 RegisterCommand('radialmenu', function()
