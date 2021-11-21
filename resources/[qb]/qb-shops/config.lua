8ec0a8ab6c204ceb31ffb24337871e1eb465eb21
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-21 03:06:41 +0100

    Removed casino bar

--- resources/[qb]/qb-shops/config.lua
+++ resources/[qb]/qb-shops/config.lua
@@ -813,9 +813,8 @@ Config.Locations = {
             [2] = vector3(962.57, 58.91, 75.43), -- Casino Kontor
             [3] = vector3(1985.29, 3053.03, 47.22), -- Yellow Jack
             [4] = vector3(-560.15, 286.88, 82.18), -- Tequila
-            [5] = vector3(936.17, 28.3, 71.83), -- Casino
-            [6] = vector3(-1586.72, -3012.67, -76.01), -- Nightclub
-            [7] = vector3(-2092.95, -1015.67, 8.98)
+            [5] = vector3(-1586.72, -3012.67, -76.01), -- Nightclub
+            [6] = vector3(-2092.95, -1015.67, 8.98)
         },
         ["products"] = Config.Products["bar"],
         ["showblip"] = true,
