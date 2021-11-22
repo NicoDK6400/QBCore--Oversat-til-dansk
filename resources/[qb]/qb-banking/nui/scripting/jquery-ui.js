0ca904b78a338c474d0360e3985aaa34dc8c3c7a
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-22 16:39:36 +0100

    Translation in banking

--- resources/[qb]/qb-banking/nui/scripting/jquery-ui.js
+++ resources/[qb]/qb-banking/nui/scripting/jquery-ui.js
@@ -9645,19 +9645,19 @@ function Datepicker() {
 	this._dayOverClass = "ui-datepicker-days-cell-over"; // The name of the day hover marker class
 	this.regional = []; // Available regional settings, indexed by language code
 	this.regional[ "" ] = { // Default regional settings
-		closeText: "Done", // Display text for close link
-		prevText: "Prev", // Display text for previous month link
-		nextText: "Next", // Display text for next month link
-		currentText: "Today", // Display text for current month link
-		monthNames: [ "January","February","March","April","May","June",
-			"July","August","September","October","November","December" ], // Names of months for drop-down and formatting
-		monthNamesShort: [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ], // For formatting
-		dayNames: [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ], // For formatting
-		dayNamesShort: [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ], // For formatting
-		dayNamesMin: [ "Su","Mo","Tu","We","Th","Fr","Sa" ], // Column headings for days starting at Sunday
-		weekHeader: "Wk", // Column header for week of the year
+		closeText: "Færdig", // Display text for close link
+		prevText: "Tidl.", // Display text for previous month link
+		nextText: "Næste", // Display text for next month link
+		currentText: "Idag", // Display text for current month link
+		monthNames: [ "Januar","Februar","Marts","April","Maj","Juni",
+			"July","August","September","Oktober","November","December" ], // Names of months for drop-down and formatting
+		monthNamesShort: [ "Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec" ], // For formatting
+		dayNames: [ "Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag" ], // For formatting
+		dayNamesShort: [ "Søn", "Man", "Tirs", "Ons", "Tors", "Fre", "Lør" ], // For formatting
+		dayNamesMin: [ "Sø","Ma","Ti","On","To","Fr","Lø" ], // Column headings for days starting at Sunday
+		weekHeader: "Ku", // Column header for week of the year
 		dateFormat: "mm/dd/yy", // See format options on parseDate
-		firstDay: 0, // The first day of the week, Sun = 0, Mon = 1, ...
+		firstDay: 1, // The first day of the week, Sun = 0, Mon = 1, ...
 		isRTL: false, // True if right-to-left language, false if left-to-right
 		showMonthAfterYear: false, // True if the year select precedes month, false for month then year
 		yearSuffix: "" // Additional text to append to the year in the month headers
@@ -9714,7 +9714,7 @@ function Datepicker() {
 	};
 	$.extend( this._defaults, this.regional[ "" ] );
 	this.regional.en = $.extend( true, {}, this.regional[ "" ] );
-	this.regional[ "en-US" ] = $.extend( true, {}, this.regional.en );
+	this.regional[ "da-DK" ] = $.extend( true, {}, this.regional.en );
 	this.dpDiv = datepicker_bindHover( $( "<div id='" + this._mainDivId + "' class='ui-datepicker ui-widget ui-widget-content ui-helper-clearfix ui-corner-all'></div>" ) );
 }
 
