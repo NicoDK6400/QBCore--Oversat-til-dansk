ca9668074170f2c886d9c331d5a3c4dab1d7a173
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-11-24 22:22:49 +0100

    Updated interior

--- resources/[qb]/qb-interior/fxmanifest.lua
+++ resources/[qb]/qb-interior/fxmanifest.lua
@@ -27,9 +27,45 @@ client_scripts {
 	'client/optional.lua'
 }
 
+exports {
+	'DespawnInterior',
+	'CreateHotel',
+	'CreateTier1House',
+	'CreateTier2House',
+	'CreateTier3House',
+	'CreateMichaelShell',
+	'CreateTrevorsShell',
+	'CreateLesterShell',
+	'CreateGunshopShell',
+	'CreateTrapHouseShell',
+	'CreateMethlabShell',
+	'CreateApartmentShell',
+	'CreateCaravanShell',
+	'CreateFranklinShell',
+	'CreateFranklinAuntShell',
+	'CreateTier1HouseFurnished',
+	'CreateHotelFurnished',
+	'CreateApartmentFurnished'
+}
+
 files {
 	'stream/defaultshells/shellprops.ytyp',
 	'stream/startingapt/shellpropsv11.ytyp',
+	'playerhouse_hotel/playerhouse_hotel.ytyp',
+	'stream/playerhouse_hotel/playerhouse_hotel.ytyp',
+	'stream/playerhouse_hotel/playerhouse_hote2.ytyp',
+	'stream/playerhouse_tier3/playerhouse_tier3.ytyp',
+	'stream/playerhouse_appartment_motel/playerhouse_appartment_motel.ytyp',
+	'stream/micheal_shell/micheal_shell.ytyp',
+	'stream/trevors_shell/trevors_shell.ytyp',
+	'stream/gunshop_shell/gunshop_shell.ytyp',
+	'stream/traphouse_shell/traphouse_shell.ytyp',
+	'stream/appartment/appartment.ytyp',
+	'stream/caravan_shell/caravan.ytyp',
+	'stream/frankelientje/frankelientje.ytyp',
+	'stream/tante_shell/tante.ytyp',
+	'stream/methlab_shell/methlab_shell.ytyp',
+	'stream/pinkcage/gabz_pinkcage.ytyp'
 	-- 'stream/ClassicHouseShells/shellpropsv19.ytyp',
 	-- 'stream/DeluxeHousingShells/shellpropsv2.ytyp',
 	-- 'stream/FurnishedHousingShells/shellpropsv11.ytyp',
@@ -54,6 +90,25 @@ files {
 -- Default (included)
 data_file 'DLC_ITYP_REQUEST' 'stream/defaultshells/shellprops.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/startingapt/shellpropsv11.ytyp'
+-- idk --
+data_file 'DLC_ITYP_REQUEST' 'stream/v_int_20.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/playerhouse_hotel/playerhouse_hotel.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/playerhouse_tier1/playerhouse_tier1.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/playerhouse_tier1/playerhouse_tier2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/playerhouse_tier3/playerhouse_tier3.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/playerhouse_appartment_motel/playerhouse_appartment_motel.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/micheal_shell/micheal_shell.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/trevors_shell/trevors_shell.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/gunshop_shell/gunshop_shell.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/traphouse_shell/traphouse_shell.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/appartment/appartment.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/caravan_shell/caravan.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/frankelientje/frankelientje.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/tante_shell/tante.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/methlab_shell/methlab_shell.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/pinkcage/gabz_pinkcage.ytyp'
+-- idk -- 
+
 -- Classic
 -- data_file 'DLC_ITYP_REQUEST' 'stream/ClassicHouseShells/shellpropsv19.ytyp'
 -- -- Deluxe
@@ -92,5 +147,6 @@ data_file 'DLC_ITYP_REQUEST' 'stream/startingapt/shellpropsv11.ytyp'
 -- data_file 'DLC_ITYP_REQUEST' 'stream/AllFurnishedShellsBundle/allfurnishedshellsbundle.ytyp'
 -- -- All Empty Bundle
 -- data_file 'DLC_ITYP_REQUEST' 'stream/AllEmptyShellsBundle/allemptyshellsbundle.ytyp'
+
 
 lua54 'yes'
