48b4b5443ea7d754fb44a7c5ba6d0fdfef1f83e3
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-12-02 14:06:12 +0100

    Added notification config

--- resources/[qb]/qb-core/config.lua
+++ resources/[qb]/qb-core/config.lua
@@ -49,3 +49,37 @@ QBConfig.Server.uptime = 0 -- Time the server has been up.
 QBConfig.Server.whitelist = false -- Enable or disable whitelist on the server
 QBConfig.Server.discord = "https://discord.gg/dkfivem" -- Discord invite link
 QBConfig.Server.PermissionList = {} -- permission list
+
+QBConfig.Notify = {}
+
+QBConfig.Notify.NotificationStyling = {
+    group = false, -- Allow notifications to stack with a badge instead of repeating
+    position = "right", -- top-left | top-right | bottom-left | bottom-right | top | bottom | left | right | center
+    progress = true -- Display Progress Bar
+}
+
+-- These are how you define different notification variants
+-- The "color" key is background of the notification
+-- The "icon" key is the css-icon code, this project uses `Material Icons` & `Font Awesome`
+QBConfig.Notify.VariantDefinitions = {
+    success = {
+        classes = 'success',
+        icon = 'done'
+    },
+    primary = {
+        classes = 'primary',
+        icon = 'info'
+    },
+    error = {
+        classes = 'error',
+        icon = 'dangerous'
+    },
+    police = {
+        classes = 'police',
+        icon = 'local_police'
+    },
+    ambulance = {
+        classes = 'ambulance',
+        icon = 'fas fa-ambulance'
+    }
+}
