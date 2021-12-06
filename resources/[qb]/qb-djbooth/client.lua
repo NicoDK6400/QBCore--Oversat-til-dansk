31b0cf2f23bbeb0645cba5db7c977ec527fc516b
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-12-06 18:05:21 +0100

    Added DJ Booth
    Added a DJ Booth script. Allows other players to listen to music.

---
+++ resources/[qb]/qb-djbooth/client.lua
@@ -0,0 +1,161 @@
+-- Variables
+
+local QBCore = exports['qb-core']:GetCoreObject()
+local currentZone = nil
+local PlayerData = {}
+
+-- Handlers
+
+AddEventHandler('onResourceStart', function(resourceName)
+	if (GetCurrentResourceName() ~= resourceName) then return end
+	PlayerData = QBCore.Functions.GetPlayerData()
+end)
+
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+    PlayerData = QBCore.Functions.GetPlayerData()
+end)
+
+RegisterNetEvent('QBCore:Client:OnJobUpdate', function(JobInfo)
+    PlayerData.job = JobInfo
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
+    PlayerData = {}
+end)
+
+-- Static Header
+
+local musicHeader = {
+    {
+        header = 'Afspil noget musik!',
+        params = {
+            event = 'qb-djbooth:client:playMusic'
+        }
+    }
+}
+
+-- Main Menu
+
+function createMusicMenu()
+    musicMenu = {
+        {
+            isHeader = true,
+            header = '💿 | DJ Pult'
+        },
+        {
+            header = '🎶 | Afspil en sang',
+            txt = 'Indtast et YouTube link',
+            params = {
+                event = 'qb-djbooth:client:musicMenu',
+                args = {
+                    zoneName = currentZone
+                }
+            }
+        },
+        {
+            header = '⏸️ | Pause musikken',
+            txt = 'Pause den nuværende sang',
+            params = {
+                isServer = true,
+                event = 'qb-djbooth:server:pauseMusic',
+                args = {
+                    zoneName = currentZone
+                }
+            }
+        },
+        {
+            header = '▶️ | Genoptag sangen',
+            txt = 'Genoptag sangen som blev sidst blev afspillet',
+            params = {
+                isServer = true,
+                event = 'qb-djbooth:server:resumeMusic',
+                args = {
+                    zoneName = currentZone
+                }
+            }
+        },
+        {
+            header = '🔈 | Ændre lydstyrke',
+            txt = 'Ændre lydstyrken på sangen/musikken',
+            params = {
+                event = 'qb-djbooth:client:changeVolume',
+                args = {
+                    zoneName = currentZone
+                }
+            }
+        },
+        {
+            header = '❌ | Sluk musikken',
+            txt = 'Stop/sluk musikken, og afspil en anden',
+            params = {
+                isServer = true,
+                event = 'qb-djbooth:server:stopMusic',
+                args = {
+                    zoneName = currentZone
+                }
+            }
+        }
+    }
+end
+
+-- DJ Booths
+
+local vanilla = BoxZone:Create(Config.Locations['vanilla'].coords, 1, 1, {
+    name="vanilla",
+    heading=0
+})
+
+vanilla:onPlayerInOut(function(isPointInside)
+    if isPointInside and PlayerData.job.name == Config.Locations['vanilla'].job then
+        currentZone = 'vanilla'
+        exports['qb-menu']:showHeader(musicHeader)
+    else
+        currentZone = nil
+        exports['qb-menu']:closeMenu()
+    end
+end)
+
+-- Events
+
+RegisterNetEvent('qb-djbooth:client:playMusic', function()
+    createMusicMenu()
+    exports['qb-menu']:openMenu(musicMenu)
+end)
+
+RegisterNetEvent('qb-djbooth:client:musicMenu', function()
+    local dialog = exports['qb-input']:ShowInput({
+        header = 'Sangvalg',
+        submitText = "Indsend",
+        inputs = {
+            {
+                type = 'text',
+                isRequired = true,
+                name = 'song',
+                text = 'YouTube URL'
+            }
+        }
+    })
+    if dialog then
+        if not dialog.song then return end
+        TriggerServerEvent('qb-djbooth:server:playMusic', dialog.song, currentZone)
+    end
+end)
+
+RegisterNetEvent('qb-djbooth:client:changeVolume', function()
+    local dialog = exports['qb-input']:ShowInput({
+        header = 'Musik lydstryke',
+        submitText = "Indsend",
+        inputs = {
+            {
+                type = 'text', -- number doesn't accept decimals??
+                isRequired = true,
+                name = 'volume',
+                text = 'Min: 0.01 - Maks: 1'
+            }
+        }
+    })
+    if dialog then
+        if not dialog.volume then return end
+        TriggerServerEvent('qb-djbooth:server:changeVolume', dialog.volume, currentZone)
+    end
+end)
