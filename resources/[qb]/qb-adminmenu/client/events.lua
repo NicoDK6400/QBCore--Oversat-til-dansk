a0e9a2dccdec1210516350ce66a8d68b77cdf4a3
NicoDK6400 <70647361+NicoDK6400@users.noreply.github.com>
2021-12-12 12:29:52 +0100

    Save HashKey instead of `veh`

--- resources/[qb]/qb-adminmenu/client/events.lua
+++ resources/[qb]/qb-adminmenu/client/events.lua
@@ -78,7 +78,7 @@ RegisterNetEvent('qb-admin:client:SaveCar', function()
         local hash = props.model
         local vehname = GetDisplayNameFromVehicleModel(hash):lower()
         if QBCore.Shared.Vehicles[vehname] ~= nil and next(QBCore.Shared.Vehicles[vehname]) ~= nil then
-            TriggerServerEvent('qb-admin:server:SaveCar', props, QBCore.Shared.Vehicles[vehname], `veh`, plate)
+            TriggerServerEvent('qb-admin:server:SaveCar', props, QBCore.Shared.Vehicles[vehname], GetHashKey(veh), plate)
         else
             QBCore.Functions.Notify('Du kan ikke gemme dette køretøj i din garage..', 'error')
         end
